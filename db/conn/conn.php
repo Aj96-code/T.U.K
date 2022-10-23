ea2ec996f7345007c5e0b0a1a2e421776d3cd4ab
Aj96-code <69065112+Aj96-code@users.noreply.github.com>
2022-10-23 00:38:11 -0500

    Change to Local db

--- db/conn/conn.php
+++ db/conn/conn.php
@@ -1,22 +1,21 @@
 <?php
     try
     {
-        /*$host = "127.0.0.1";
+        $host = "127.0.0.1";
         $db = "tuk_db";
         $dbUser = "phpuser";
         $dbPassword = 'PHPU$3rP@$$w0rd';
         $charset = "utf8mb4";
         //* data source name
         $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-        */
-         //Remote Connect
+        /* Remote Connect
         $host ="sql.freedb.tech";
         $db = "freedb_aj96codedb";
         $dbUser = "freedb_freedb_aj96codedb";
         $dbPassword = "FA5FNpf&geJXV$2";
         $charset = "utf8mb4";
         $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-            
+        */    
     
         $pdo = new PDO($dsn,$dbUser,$dbPassword);
         $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
