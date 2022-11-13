91fc4026417c95895982dbddb735c86975566554
Aj96-code <69065112+Aj96-code@users.noreply.github.com>
2022-11-13 02:01:17 -0500

    Fix Search error

--- layouts/views/product/product-list.php
+++ layouts/views/product/product-list.php
@@ -1,6 +1,6 @@
 <section>
         <div class="container d-flex d-sm-flex justify-content-center align-items-center justify-content-sm-center align-items-sm-center my-3">
-            <form class="d-inline-flex" method="post"><input class="form-control form-control-sm me-1 search-input" name="search-phrase" type="text" placeholder="Search ..."><button class="btn btn-dark px-2 py-0" type="submit" style="padding: 0px;"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-search fs-5 text-light px-0 pe-0">
+            <form class="d-inline-flex" method="post" action="/products"><input class="form-control form-control-sm me-1 search-input" name="search-phrase" type="text" placeholder="Search ..."><button class="btn btn-dark px-2 py-0" type="submit" style="padding: 0px;"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-search fs-5 text-light px-0 pe-0">
                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
                     </svg></button></form>
         </div>
